3340755de81ca599bd64e0c60b7f1e31954816d3
João Pedro Souza de Lima <jpedrol42@gmail.com>
2026-01-15 16:37:46 -0300

    trigger pra atualizar saldo na caixinha compartilhada

--- nu_turbo/automatizacoes.sql
+++ nu_turbo/automatizacoes.sql
@@ -1,6 +1,26 @@
 --saldo atualizada da caixinha apos movimentação
 create or replace FUNCTION
-atualiza_saldo_caixinha()
+fn_atualiza_saldo_caixinha()
 returns trigger as $$
 begin
-     if (new.tipo_movimento = 'deposito')
+     if (new.tipo_movimento = 'deposito') THEN
+     update caixinha
+     set saldo_caixinha = saldo_caixinha + new.valor
+     where id_caixinha = new.fk_caixinha;
+
+        elsif (new.tipo_movimento = 'pagamento' or new.tipo_movimento = 'resgate') THEN
+        update caixinha
+        set saldo_caixinha = saldo_caixinha - new.valor
+        where id_caixinha = new.fk_caixinha;
+    end if;
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger
+trg_atualiza_saldo_caixinha
+after insert on movimentacao_caixinha
+for each row
+execute function fn_atualiza_saldo_caixinha();
+
+
