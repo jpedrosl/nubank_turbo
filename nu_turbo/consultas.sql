ab6864c913895debb81d67e24d7896b1ad65ba53
Nicolas <devnicolassilva@gmail.com>
2026-01-15 16:42:33 -0300

    consultas atualizadas

--- nu_turbo/consultas.sql
+++ nu_turbo/consultas.sql
@@ -7,4 +7,12 @@ join contas on fk_cliente = id_cliente;
 
 -- Exibi o nome dos clientes e o limite total, o limite utilizado e o limite ainda disponível.
 select nome,limite_credito,limite_utilizado from clientes c 
-join cartao on id_cartao = id_cliente email
+join cartao on id_cartao = id_cliente;
+
+
+-- selecionar os clientes com o saldo maior que 5.000
+select nome, saldo_disponivel from clientes c
+join contas on fk_cliente = id_cliente
+where saldo_disponivel > 5000 
+
+
