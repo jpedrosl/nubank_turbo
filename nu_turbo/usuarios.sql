fe4c9ed9881f2f4e2b73e9b02eec4df55e1a486d
João Pedro Souza de Lima <jpedrol42@gmail.com>
2026-01-16 15:05:51 -0300

    criaçao do index e permissoes para adm e analista

--- nu_turbo/usuarios.sql
+++ nu_turbo/usuarios.sql
@@ -7,4 +7,5 @@ grant all privileges on all sequences in schema  public to admin_nubank;
 
 
 
-grant select on clientes, contas, extrato, caixinha, movimentacao_caixinha to analista_suporte;
+grant select on clientes, contas, extrato, caixinha, movimentacao_caixinha on analista_suporte;
+
