773822c641a052b546cc42ae25615c324d75e9e5
Pedro Souza <jpedrosl42@gmail.com>
2026-01-16 00:22:44 -0300

    automatizacoes.1

--- nu_turbo/automatizacoes.sql
+++ nu_turbo/automatizacoes.sql
@@ -24,3 +24,36 @@ for each row
 execute function fn_atualiza_saldo_caixinha();
 
 
+--tabela de log que armazena as movimentações realizadas na caixinha
+create or replace function fn_log_limite_cartao()
+returns trigger as $$
+begin
+    if (old.limite_credito <> new.limite_credito) THEN
+    insert into log_limite_cartao (fk_cartao, limite_antigo, limite_novo)
+    values (old.id_cartao, old.limite_credito, new.limite_credito);
+    end if;
+    return new;
+end;
+$$ language plpgsql;
+
+create trigger
+trg_log_limite
+after update on cartao
+for each row
+execute function fn_log_limite_cartao();
+
+
+--todo email de cliente e salvo em letras minusculas
+create or replace function fn_tratar_email_cliente()
+returns trigger as $$
+begin
+    new.email := lower(new.email);
+    return new;
+end;
+$$ language plpgsql;
+
+create TRIGGER
+trg_padroniza_email
+before insert or update on clientes
+for each row
+execute function fn_tratar_email_cliente();
